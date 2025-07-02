2e7c06a17ff44530121c4676627b060919b00af3
jorge.cruz <jorge.cruz@datadoghq.com>
2025-07-01 18:24:17 -0600

    Update terraform outputs

--- main.tf
+++ main.tf
@@ -72,3 +72,12 @@ resource "aws_key_pair" "nginx_key" {
   }
 }
 
+output "server_public_ip" {
+    value = aws_instance.nginx-server.public_ip
+    description = "The public IP address of the Nginx server instance"
+}
+
+output "server_public_dns" {
+    value = aws_instance.nginx-server.public_dns
+    description = "The public IP address of the Nginx server instance"
+}
